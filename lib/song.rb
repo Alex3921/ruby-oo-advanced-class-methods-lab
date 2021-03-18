4e6ec30cd6d8ee149d04acbcda46a1d28b0c6db5
alexandru <boncut.alex10@gmail.com>
2021-03-17 21:52:02 -0500

    commit tree 0787b1f154e4621d188c86e87db000372bc4fbff

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +7,64 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+  
   def save
     self.class.all << self
+  end
+
+  # song = Song.create_by_name("Blank Space")
+  # songa = Song.create_by_name("Blaank Space")
+  # binding.pry
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    data = filename.delete_suffix(".mp3").split(" - ")
+    song = self.new
+    song.name = data[1]
+    song.artist_name = data[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    data = filename.delete_suffix(".mp3").split(" - ")
+    song = self.new
+    song.name = data[1]
+    song.artist_name = data[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
